042d47bc167007ad05b51e33a662d07ddb7e545f
lukas <lucass.lv.lv@gmail.com>
2022-05-17 14:53:22 -0300

    box content

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Box from '@mui/material/Box';
 
+// Themes
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
-
 import GlobalStyles from './Styles/global';
 
+// default Styles
 import MUITheme from './Styles/Default/MUI';
 import SCTheme from './Styles/Default/Styled';
 
-import Routes from './Routes';
+// Componentes
+import { DrawerHeader } from './components/DrawerHeader';
 import LateralMenu from './components/LateralMenu';
+import Routes from './Routes';
 
 function App() {
   return (
@@ -18,8 +22,14 @@ function App() {
       <StyledThemeProvider theme={SCTheme}>
         <GlobalStyles />
         <BrowserRouter>
-          <LateralMenu />
-          <Routes />
+          <Box sx={{ display: 'flex' }}>
+            <LateralMenu />
+
+            <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+              <DrawerHeader />
+              <Routes />
+            </Box>
+          </Box>
         </BrowserRouter>
       </StyledThemeProvider>
     </MUIThemeProvider>
