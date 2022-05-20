b1171ba9c05223f8172da3c9eff3327476007cb6
lukas <lucass.lv.lv@gmail.com>
2022-05-20 18:42:16 -0300

    componente de regards

--- src/components/Regards/index.tsx
+++ src/components/Regards/index.tsx
@@ -1,9 +1,30 @@
+/* eslint-disable object-curly-newline */
 import React from 'react';
 
-import { Container } from './styles';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import salesdashboard from '../../Assets/images/salesdashboard.svg';
+import { Container, Title, Subtitle, Prize } from './styles';
 
 function Regards() {
-  return <Container>Regards</Container>;
+  return (
+    <Container>
+      <div>
+        <Title>Congratulations Watson! 🎉</Title>
+        <Subtitle>You have done 76% more sales today.</Subtitle>
+        <Subtitle>Check your inventory and update your stocks.</Subtitle>
+
+        <Prize>
+          <WorkspacePremiumIcon className='icon' />
+
+          <div className='analytics'>
+            <span className='StarSeller'>Star Seller</span>
+            <span className='percentage'>76%</span>
+          </div>
+        </Prize>
+      </div>
+      <img src={salesdashboard} alt='seller' />
+    </Container>
+  );
 }
 
 export default Regards;
--- src/components/Regards/styles.ts
+++ src/components/Regards/styles.ts
@@ -7,4 +7,72 @@ export const Container = styled.div`
   box-shadow: 0px 2px 2px rgba(29, 36, 56, 0.1);
   border-radius: 5px;
   background: ${({ theme }) => theme.palette.primary.light};
+  padding: 50px 30px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Title = styled.span`
+  display: block;
+  font-size: 18px;
+  color: ${({ theme }) => theme.palette.primary.contrastText};
+`;
+
+export const Subtitle = styled.span`
+  display: block;
+  font-size: 12px;
+  font-weight: 600;
+  color: #acb8d1;
+`;
+
+export const Prize = styled.div`
+  width: 100%;
+  height: 50px;
+  border-radius: 10px;
+  background: ${({ theme }) => theme.palette.primary.main};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+
+  .icon {
+    fill: #fff;
+    font-size: 25px;
+  }
+
+  .analytics {
+    width: 80%;
+    height: 20px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .StarSeller,
+    .percentage {
+      font-size: 14px;
+      color: #fff;
+    }
+
+    &::before {
+      content: '';
+      z-index: 2;
+      margin-top: 20px;
+      width: 80%;
+      height: 4px;
+      background: #fff;
+      position: absolute;
+    }
+
+    &::after {
+      content: '';
+      margin-top: 20px;
+      width: 100%;
+      height: 4px;
+      background: #7cc2f6;
+      position: absolute;
+    }
+  }
 `;
